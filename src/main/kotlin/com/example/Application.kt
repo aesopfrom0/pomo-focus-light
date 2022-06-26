fd8edae58ddc1fd58b5c8e6aa037ebf6be90611d
Aesop <ee.changsub@gmail.com>
2022-06-27 00:00:58 +0900

    Initial Commit

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,14 @@
+package com.example
+
+import com.example.plugins.configureMonitoring
+import com.example.plugins.configureRouting
+import com.example.plugins.configureSerialization
+import io.ktor.server.application.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+    configureMonitoring()
+}
