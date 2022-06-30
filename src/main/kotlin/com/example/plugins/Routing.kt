4ed36b249e58c97165661a972a7644af66e3d794
Aesop <ee.changsub@gmail.com>
2022-06-30 22:33:36 +0900

    chore: routing > post practice

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -6,14 +6,32 @@ import io.ktor.server.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class User(
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val phoneNumber: String
+)
 
 fun Application.configureRouting() {
-    
-
     routing {
         get("/") {
             call.respondText("Hello AESOP!")
         }
+        post("/users") {
+            val user = call.receive<User>()
+            println(user)
+
+            val (firstName) = user
+
+            println("Have a nice day, $firstName!")
+
+            call.respondText("Have a nice day, $firstName!")
+        }
+
         // Static plugin. Try to access `/static/index.html`
         static("/static") {
             resources("static")
